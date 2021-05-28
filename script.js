9103a9dcc3fbede5615b282b9292c718bbfc4f97
akyravish <info.akshayravish@gmail.com>
2021-05-28 15:21:56 +0530

    add draw Stroke funtion

--- script.js
+++ script.js
@@ -13,6 +13,12 @@ const drawCirlce = (x, y) => {
 
 const drawStroke = (x1, y1, x2, y2) => {
 	ctx.beginPath();
+	ctx.moveTo(x1, y1);
+	ctx.lineTo(x2, y2);
+	ctx.strokeStyle = color;
+	ctx.lineWidth = size;
+	ctx.stroke();
 };
 
 drawCirlce(200, 200);
+drawStroke(300, 200, 300, 500);
