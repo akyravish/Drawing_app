784763fac5a0fcea97049a318e7301b9cc0fe1f4
akyravish <info.akshayravish@gmail.com>
2021-05-28 15:13:20 +0530

    add draw circle function

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 
+color = 'black';
+size = 20;
+
+const drawCirlce = (x, y) => {
+	ctx.beginPath();
+	ctx.arc(x, y, size, 0, Math.PI * 2);
+	ctx.fillStyle = color;
+	ctx.fill();
+};
+
+const drawStroke = (x1, y1, x2, y2) => {
+	ctx.beginPath();
+};
+
+drawCirlce(200, 200);
